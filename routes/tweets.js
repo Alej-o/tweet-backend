41a02f917ab1cdeb8a927a35d4a71fd7f8478cd5
Nicolas Portier <nicola.portier@gmail.com>
2025-02-14 14:24:02 +0100

    Update: Aggregate Tweet

--- routes/tweets.js
+++ routes/tweets.js
@@ -56,7 +56,7 @@ router.delete("/:id", async (req, res) => {
         const user = await User.findOne({ token: token });
         if (user === null) return res.json({ result: false, error: "No user matching the token!" });
 
-        const deleteTweetById = await Tweet.findOneAndDelete({ _id: id, user: {_id: user._id} });
+        const deleteTweetById = await Tweet.findOneAndDelete({ _id: id, user: { _id: user._id } });
         if (deleteTweetById === null) return res.json({ result: false, error: "Tweet not found" });
 
         return res.json({ result: true });
@@ -68,8 +68,50 @@ router.delete("/:id", async (req, res) => {
 });
 //
 router.get("/", async (req, res) => {
-    const allTweet = await Tweet.find().populate("user").sort({ createdAt: -1 })
-    res.json({ tweets: allTweet });
+    const { token } = req.query;
+
+    const user = await User.findOne({token: token}).select('_id');
+
+    const tweets = await Tweet.aggregate([
+        {
+            $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user",
+            },
+        },
+        { $unwind: "$user" },
+        {
+            $lookup: {
+                from: "likes",
+                let: { tweetId: "$_id" },
+                pipeline: [
+                    { $match: { $expr: { $and: [{ $eq: ["$tweetId", "$$tweetId"] }, { $eq: ["$userId", user._id] }] } } },
+                    { $limit: 1 },
+                ],
+                as: "likedByUser",
+            },
+        },
+        {
+            $addFields: {
+                isLiked: { $gt: [{ $size: "$likedByUser" }, 0] }, 
+            },
+        },
+        {
+            $project: {
+                content: 1,
+                like: 1,
+                user: { token : 1, username: 1, firstname: 1 }, 
+                isLiked: 1,
+                createdAt: 1,
+            },
+        }, {
+            $sort: { createdAt: -1 }
+        }
+    ]);
+    // const allTweet = await Tweet.find().populate("user").sort({ createdAt: -1 })
+    res.json({ tweets: tweets });
 
 });
 
