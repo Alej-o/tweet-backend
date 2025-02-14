9b4cdebd878cb0de9c1918e740b1d4d96d9cf6fd
Nicolas Portier <nicola.portier@gmail.com>
2025-02-14 11:18:44 +0100

    change json return tweetByHashtag

--- routes/hashtags.js
+++ routes/hashtags.js
@@ -3,48 +3,47 @@ var router = express.Router();
 const Hashtags = require('../models/hashtags');
 
 
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
 	const allHashtags = await Hashtags.find();
-  const allHashtagsData = allHashtags.map(hashtag => ({
-    name: hashtag.name,
-    tweetCount: hashtag.tweets.length
-}))
-    .sort((a, b) => b.tweetCount - a.tweetCount)
-    .slice(0, 10); 
-		res.json({ hashtags: allHashtagsData});
-	
+	const allHashtagsData = allHashtags.map(hashtag => ({
+		name: hashtag.name,
+		tweetCount: hashtag.tweets.length
+	}))
+		.sort((a, b) => b.tweetCount - a.tweetCount)
+		.slice(0, 10);
+	res.json({ hashtags: allHashtagsData });
+
 });
 
 router.get("/tweetByHashtag", async (req, res) => {
- 
-    let { hashtag } = req.query; 
 
-    if (!hashtag) {
-      return res.json({ error: "Hashtag requis" });
-    }
-    if (!hashtag.startsWith("#")) {
-      hashtag = `#${hashtag}`;
-  }
-    const hashtagData = await Hashtags.findOne({ name: hashtag });
+	let { hashtag } = req.query;
 
-    if (!hashtagData) {
-      return res.json({ error: "Hashtag non trouvé" });
-    }
-   
-    const populatedHashtag = await Hashtags.findById(hashtagData._id)
-    .populate({
-      path: 'tweets',
-      populate: {
-        path: 'user',
-        select: 'username firstname'
-      }
-    })
-    populatedHashtag.tweets.sort((a, b) => b.createdAt - a.createdAt);
-    res.json({  populatedHashtag });
- 
+	if (!hashtag) {
+		return res.json({ error: "Hashtag requis" });
+	}
+	if (!hashtag.startsWith("#")) {
+		hashtag = `#${hashtag}`;
+	}
+	const hashtagData = await Hashtags.findOne({ name: hashtag });
+
+	if (!hashtagData) {
+		return res.json({ error: "Hashtag non trouvé" });
+	}
+
+	const populatedHashtag = await Hashtags.findById(hashtagData._id)
+		.populate({
+			path: 'tweets',
+			populate: {
+				path: 'user',
+				select: 'username firstname'
+			}
+		})
+	populatedHashtag.tweets.sort((a, b) => b.createdAt - a.createdAt);
+	res.json({ result: true, tweets: populatedHashtag.tweets });
 });
 
 
 
- 
-  module.exports = router;
+
+module.exports = router;
